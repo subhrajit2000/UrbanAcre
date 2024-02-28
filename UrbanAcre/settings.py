218e061a4e2de7704f3268119844094136afcfe6
subhrajit2000 <nayaksubhrajit84@gmail.com>
2024-02-29 01:04:38 +0530

    realtors app created

--- UrbanAcre/settings.py
+++ UrbanAcre/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'useraccount',
+    'realtors',
+    'listings',
 ]
 
 MIDDLEWARE = [
--- UrbanAcre/urls.py
+++ UrbanAcre/urls.py
@@ -26,6 +26,8 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/useraccount/', include('useraccount.urls')),
+    path('api/realtors/', include('realtors.urls')),
+    path('api/listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
